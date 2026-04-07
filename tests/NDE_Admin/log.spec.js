1699a76aa6e659b20faf1e148d7fd62d199557ab
Krishnasanjay <Krishnasanjay1012@gmail.com>
2026-04-07 14:20:51 +0530

    NDE Admin Login

---
+++ tests/NDE_Admin/log.spec.js
@@ -0,0 +1,12 @@
+const {test,expect}=require("@playwright/test")
+test.setTimeout(5 * 30 * 1000)
+
+test("practice",async ({ page })=>{
+  await page.goto("https://superadmin.nowdigitaleasy.com/login")
+await page.locator('[name="userName"]').fill("iaaxin")
+await page.locator('[name="password"]').fill("King_Guna")
+await page.getByRole("button",{name:"Login"}).click()
+await page.waitForTimeout(3000)
+await expect(page).toHaveURL("https://superadmin.nowdigitaleasy.com/home")
+console.log("login passed")
+})
